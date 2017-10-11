cb50a60fadebe146280d5b29015ceb42d677c2b1
ercjns <eric@ercjns.com>
2017-10-11 11:50:22 -0700

    mailman edits

--- losttime/mailman.py
+++ losttime/mailman.py
@@ -10,15 +10,11 @@ def send_email(sender, recipient, subject, body, verbose=False):
     message = header + body
 
     if app.config["SEND_EMAILS"]:
-        p = Popen(['/usr/sbin/sendmail', "-t"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p = Popen(['/usr/bin/sendmail', "-t"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         res = p.communicate(message)
     else:
         res = message
 
     if verbose:
-        print('Process completed with {}'.format(res))
+        print('Process completed with: {}\n'.format(res))
     return res
-
-
-if __name__ == '__main__':
-    print(send_email('bot@example.com', 'eric@ercjns.com', 'TestMail', 'This is a test message.', True))
